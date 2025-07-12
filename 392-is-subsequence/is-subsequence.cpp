41bf88641085cea3678a4d44cbac4d00fec6e098
HetSoni04 <sonihet90@gmail.com>
2025-07-12 22:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (90.16%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int it = 0;
-        int js = 0;
-        if(s == t){
-            return true;
+        int same = 0;
+        int i = 0;
+        if(s == t) return true;
+        if(s == "") return true;
+
+        while(t[i] != '\0'){\
+            if(s[same] == t[i]) same++;
+            i++;
         }
-        if(s == ""){
-            return true;
-        }
-        int s_len = s.size();
-        while(t[it] != '\0' && s[js] != '\0'){
-            if(t[it] == s[js]){
-                string temp = t.substr(0,s_len);
-                if(temp == s){
-                    return true;
-                }
-                js++;
-            }
-            else{
-                t.erase(it,1);
-                it--;
-            }
-            it++;
-        }
-        if(t == s){
-            return true;
-        }
+
+        if(s.size() == same) return true;
         return false;
+
+        // int it = 0;
+        // int js = 0;
+        // if(s == t){
+        //     return true;
+        // }
+        // if(s == ""){
+        //     return true;
+        // }
+        // int s_len = s.size();
+        // while(t[it] != '\0' && s[js] != '\0'){
+        //     if(t[it] == s[js]){
+        //         string temp = t.substr(0,s_len);
+        //         if(temp == s){
+        //             return true;
+        //         }
+        //         js++;
+        //     }
+        //     else{
+        //         t.erase(it,1);
+        //         it--;
+        //     }
+        //     it++;
+        // }
+        // if(t == s){
+        //     return true;
+        // }
+        // return false;
     }
 };
