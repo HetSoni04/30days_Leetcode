c4d7e35350be1b3ba0ece9e25269c536114e96ec
HetSoni04 <sonihet90@gmail.com>
2025-07-22 18:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (58.21%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,14 +3,50 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int s = nums.size();
         vector<int> res;
-        for(int i=0;i<s;i++){
-            for(int j=i+1;j<s;j++){
-                if((nums[i]+nums[j]) == target){
-                    res.push_back(i);
-                    res.push_back(j);
-                    return res;
-                }
+        // for(int i=0;i<s;i++){
+        //     for(int j=i+1;j<s;j++){
+        //         if((nums[i]+nums[j]) == target){
+        //             res.push_back(i);
+        //             res.push_back(j);
+        //             return res;
+        //         }
+        //     }
+        // }
+
+        //two pointer's approach --> O(nlogn) -- pair + sort + twopointers
+        // vector<pair<int,int>> arr;
+        // for(int i = 0; i<s ;i++){
+        //     arr.push_back({nums[i],i});
+        // }
+        // sort(arr.begin(),arr.end());
+        // int left = 0;
+        // int right = s-1;    
+        // while(left < right){
+        //     int sum = arr[left].first + arr[right].first;
+        //     if(sum == target){
+        //         res.push_back(arr[left].second);
+        //         res.push_back(arr[right].second);
+        //         break;
+        //     }
+        //     else if(sum < target){
+        //         left++;
+        //     } 
+        //     else{
+        //         right--;
+        //     }
+        // }
+
+
+        //Hashmap approach -- Best approach -- O(n)
+        unordered_map<int,int> m;
+        for(int i = 0; i<s; i++){
+            int comp = target - nums[i];
+            if(m.find(comp) != m.end()){
+                res.push_back(m[comp]);
+                res.push_back(i);
+                break;
             }
+            m[nums[i]] = i;
         }
         return res;
     }
